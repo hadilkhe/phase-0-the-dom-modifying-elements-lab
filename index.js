4f27674b2ba08da4388a0bcaad575b932ccc0971
hadilkhe <121692875+hadilkhe@users.noreply.github.com>
2023-10-02 22:28:04 +0100

    finished

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const newDiv = document.createElement("div");
+newDiv.textContent = "This is a dynamically created div.";
+document.body.appendChild(newDiv);
+
+
+const ul = document.createElement("ul");
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+    ul.appendChild(li);
+  }
+  newDiv.appendChild(ul);
+  
